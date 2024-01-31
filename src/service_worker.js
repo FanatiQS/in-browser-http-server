e4cae3bf762fdbe396201c4d085574fecfdb70b3
FanatiQS <fanatiqs.com@gmail.com>
2024-01-31 19:58:53 +0100

    Use mime type from file.type

--- src/service_worker.js
+++ src/service_worker.js
@@ -8,36 +8,15 @@ addEventListener("message", function (event) {
 	event.source.postMessage("initialized");
 });
 
-// Gets content type from file extension
-function getContentType(path) {
-	switch (path.slice(path.lastIndexOf(".") + 1)) {
-		case "html": return "text/html";
-		case "js":   return "text/javascript";
-		case "css": return "text/css";
-
-		case "json": return "application/json";
-
-		case "jped": return "image/jpeg";
-		case "jpg": return "image/jpeg";
-		case "png": return "image/png";
-		case "svg": return "image/svg+xml";
-
-		case "ttf": return "font/ttf";
-
-		default: return "text/plain";
-	}
-}
-
 // Reads files content from file system
 async function readFile(path) {
 	console.log("Service worker fetch response:", path);
 	const fileHandle = await fs.getFileHandle(path);
 	const file = await fileHandle.getFile();
-	const mime = getContentType(path);
-	console.log("Service worker fetch mime type:", mime);
+	console.log("Service worker fetch mime type:", file.type);
 	return new Response(await file.text(), {
 		headers: {
-			"Content-Type": mime
+			"Content-Type": file.type
 		}
 	});
 }
